ca10c255396a73f8b4b95d729c4b890d2d0aedd2
noufanelachola <noufansadiqelachola@gmail.com>
2024-02-15 01:03:34 +0530

    two routes added

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from fastapi import FastAPI
+from models import Todo
 
 app = FastAPI()
 
+todos = []
 
-@app.get("/")
-async def root():
-    return{
-        "message":"hello..noufu did it"
-    }
+# Get all todos
+@app.get("/todos/get")
+async def getAllTodos():
+    return{"todosList":todos}
 
+# Create a todo
+@app.post("/todo/create")
+async def createTodo(todo:Todo):
+    todos.append(todo)
+    return{"message":"Todo has been successfully added"}
+
+# 
+@app.get("/todo/get/{todoId}")
+async def getTodo(todoId:int):
+    for todo in todos:
+        if todo.id == todoId :
+            return {"todo":todo}
